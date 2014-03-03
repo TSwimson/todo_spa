1b0c0d72b3075502a0e5cdcbf18f4196a692f6d8
Tanner Swenson <TannerSwenson@gmail.com>
2014-03-03 11:42:46 -0800

    Showing todos

--- app/assets/javascripts/views/todos/todos_index.js
+++ app/assets/javascripts/views/todos/todos_index.js
@@ -1,6 +1,9 @@
 SpaApp.Views.TodosIndex = Backbone.View.extend({
+  events: {
+    'submit #addTodo'   :'add_todo',
+    'click .delete_todo':'delete_todo'
+  },
   id: 'todos',
-
   template: HandlebarsTemplates['todos/index'],
 
   render: function() {
@@ -11,7 +14,27 @@ SpaApp.Views.TodosIndex = Backbone.View.extend({
       this.$el.append(todoHTML);
     }, this);
 
-    return this;
-  }
+    $('#addTodo').on('submit', function(event) {
+      event.preventDefault();
 
+    });
+    return this;
+  },
+
+  add_todo: function(event) {
+    var _this = this;
+    event.preventDefault();
+    var new_todo = {
+      title: $('#todo_title').val(),
+      completed: false
+    };
+    $.ajax({
+      method: 'post',
+      url   : '/todos.json',
+      data  : {todo: new_todo}
+    }).done(function(responseData){
+      var todoHTML = HandlebarsTemplates['todos/show'](responseData);
+      _this.$el.append(todoHTML);
+    });
+  }
 });
